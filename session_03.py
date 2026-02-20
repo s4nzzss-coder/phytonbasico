d663783fa6a7763320f9a4f2c4771762f62a9315
s4nzzss-coder <s4nzzss@gmail.com>
2026-02-20 15:58:32 -0800

    guarda este cambio en mi phytanbasico

--- session_03.py
+++ session_03.py
@@ -48,8 +48,8 @@ while i < 5:
 
 i = 0
 mi_lista_2 = ["lunes", "martes", "miercoles","jueves", "viernes"]
-while 1 < 3:
-   i +=1
-   for d in mi_lista_2:
-    if d !="lunes":
+i < 3
+i +=1
+for i in range (3):
+ for d in mi_lista_2:
       print (d)
