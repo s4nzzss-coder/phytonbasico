2796a7203d6fafb4d5b0948c8141265fb699569d
s4nzzss-coder <s4nzzss@gmail.com>
2026-02-20 15:41:32 -0800

    guarda este cambio

--- session_03.py
+++ session_03.py
@@ -44,10 +44,4 @@ while i < 5:
 # Miercoles
 # Jueves 
 # viernes
-i = 0
-mi_lista_2 = ["lunes", "martes", "miercoles","jueves", "viernes"]
-while 1 < 3:
-   i += 1
-   for d in mi_lista_2:
-    if d !="lunes":
-      print (f"{d}!")
+
